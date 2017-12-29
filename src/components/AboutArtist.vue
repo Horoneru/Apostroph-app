c11cdb624d4dfc80f31438b7048806382b1e75c9
Horoneru <horonerudev@gmail.com>
2017-12-29 15:29:38 +0100

    Ajoute les versions initiales des pages d'à-propos

---
+++ src/components/AboutArtist.vue
@@ -0,0 +1,27 @@
+<template>
+  <double-pane-layout :title="artist.name" :leftImage="artist.image" imageAlign="bottom" :contentClasses="'gradient-' + this.artist.theme">
+    <p slot="rightContent">{{ artist.desc }}</p>
+  </double-pane-layout>
+</template>
+
+<script>
+import DoublePaneLayout from './DoublePaneLayout';
+import artists from '../service/ArtistProvider.js';
+
+export default {
+  name: 'AboutArtist',
+  props: ['artistId'],
+  components: { DoublePaneLayout },
+  data: function() {
+    return {
+      artist: null
+    };
+  },
+  created: function() {
+    this.artist = artists[this.artistId];
+  }
+};
+</script>
+
+<style scoped>
+</style>
