6cda1b0df4b4bc2be8d0dea2fbe1717c88e5fb9f
Horoneru <horonerudev@gmail.com>
2018-01-08 20:44:37 +0100

    Utilise le nouveau composant maintenant réutilisable

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -6,52 +6,30 @@
     <h2 class="subtitle">Choisissez un mini jeu</h2>
     <hr>
     <el-row class="menu-container" type="flex" justify="space-around" >
-      <router-link tag="a" :to="{ name: 'gameintro', params: { game: 'cryptography' } }">
-        <el-row class="flex-column p-4 m-x-3 bordered-menu gradient-cryptography-nav" type="flex" justify="center" align="middle">
-          <img class="game-icon" src="/../static/assets/icon-chiffr.png">
-          <el-button type="text">Cryptographie</el-button>
-        </el-row>
-      </router-link>
-      <router-link tag="a" :to="{ name: 'gameintro', params: { game: 'programming' } }">
-        <el-row class="flex-column p-4 m-x-3 bordered-menu gradient-programming-nav" type="flex" justify="center" align="middle">
-          <img class="game-icon" src="/../static/assets/icon-prog.png">
-          <el-button type="text">Programmation</el-button>
-        </el-row>
-      </router-link>
+      <nav-button text="Cryptographie" :to="{ name: 'gameintro', params: { game: 'cryptography' } }"
+      classes="gradient-cryptography-nav" icon="/../static/assets/icon-chiffr.png">
+      </nav-button>
+      <nav-button
+      text="Programmation" :to="{ name: 'gameintro', params: { game: 'programming' } }"
+      classes="gradient-programming-nav" icon="/../static/assets/icon-prog.png">
+      </nav-button>
     </el-row>
   </el-row>
 </template>
 
 <script>
-  export default {
-    name: 'Menu'
-  };
+import NavButton from './NavButton';
+export default {
+  components: {
+    NavButton
+  },
+  name: 'Menu'
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .bordered-menu {
-    border: 1px white solid;
-    border-radius: 5px;
-  }
-
-  .game-icon {
-    width: 72px;
-    height: 72px;
-  }
-
-  .el-button--text {
-    font-size: 18px;
-    color: white;
-    text-shadow: 0px 1px 20px rgba(0, 0, 0, 0.25);
-  }
-
-  a {
-    text-decoration: none;
-  }
-
   .menu-container {
     width: 40%;
   }
-
 </style>
