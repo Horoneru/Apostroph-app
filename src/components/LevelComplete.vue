48de935a22cf2db359f6e1ee118fe3ae6dc66853
Horoneru <horonerudev@gmail.com>
2018-01-10 21:17:07 +0100

    Enlève le . du niveau terminé

--- src/components/LevelComplete.vue
+++ src/components/LevelComplete.vue
@@ -6,7 +6,7 @@
       <hr>
     </div>
     <div>
-      <h1 v-if="levelid !== 'tutorial'">Niveau {{ levelid }} terminé.</h1>
+      <h1 v-if="levelid !== 'tutorial'">Niveau {{ levelid }} terminé</h1>
       <h1 v-else>Tutoriel terminé ! </h1>
       <hr>
       <small v-if="levelid === '1'">Preview terminée aussi</small>
@@ -39,36 +39,17 @@
 <style scoped>
 
 hr {
-  width : 40% ; 
+  width : 40% ;
   margin : auto ;
   margin-top : 20px ;
 }
 
 .btnLvlComplete {
-  margin-top : 30px ; 
+  margin-top : 30px ;
 }
 
 .btnLvlComplete img{
-  width : 75px ; 
-  height :  auto ; 
+  width : 75px ;
+  height :  auto ;
 }
-
- /*Burger Menu */
-
-  .burgerMenu {
-    position: absolute ; 
-    top : 15px ;
-    left: -10px;
-    width : 125px ; 
-    height : auto ; 
-  }
-
-  .burgerMenu hr{
-    width : 50px ; 
-    height : 6px ; 
-    margin-bottom: 10px;
-    margin-top: 10px;
-  }
-
-
 </style>
