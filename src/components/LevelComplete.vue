f139bce20a945faa66de27ee3d005628fc5af52d
Horoneru <horonerudev@gmail.com>
2018-01-14 16:59:14 +0100

    Utilise une référence au niveau suivant

--- src/components/LevelComplete.vue
+++ src/components/LevelComplete.vue
@@ -12,7 +12,7 @@
       <div class="btn-lvl-complete animated fadeInUpBig">
         <router-link v-ripple tag="a" :to="{ name: 'aboutartist', params: { artistid: game.levels[levelid].artist.id } }"><el-button type="text"><img src="../../static/assets/artiste.png"></el-button></router-link>
         <router-link v-ripple tag="a" :to="{ name: 'levelselect', params: { gameid } }"><el-button type="text"><img src="../../static/assets/home.png"></el-button></router-link>
-        <router-link v-ripple tag="a" :to="{ name: 'levelintro', params: { gameid, 'levelid': '1' } }"><el-button type="text"><img src="../../static/assets/suite.png"></el-button></router-link>
+        <router-link v-ripple tag="a" :to="{ name: 'levelintro', params: { gameid, 'levelid': game.levels[levelid].next } }"><el-button type="text"><img src="../../static/assets/suite.png"></el-button></router-link>
       </div>
     </div>
   </el-row>
