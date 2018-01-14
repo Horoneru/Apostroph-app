0eb55483b3ba7cfa83d7e8113c70dd26170171db
Horoneru <horonerudev@gmail.com>
2018-01-14 15:05:31 +0100

    Refactor les validations récurrentes dans leur propre fichier

---
+++ src/service/ValidatorProvider.js
@@ -0,0 +1,18 @@
+import games from './GameProvider';
+import artists from './ArtistProvider';
+const validators = {
+  game: function(game) {
+    return game in games;
+  },
+  level: function(game, level) {
+    return level in games[game];
+  },
+  artist: function(artist) {
+    return artist in artists;
+  },
+  piece: function(artist, piece) {
+    return piece in artists[artist].pieces;
+  }
+};
+
+export default validators;
