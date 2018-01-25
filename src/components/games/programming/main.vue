48110b077b3ec3cb6e54a9484245c9e1f5f3f04e
Horoneru <horonerudev@gmail.com>
2018-01-25 20:38:21 +0100

    Adjuste l'opacité du timeline-container
    Basé sur le feedback de Thomas

--- src/components/games/programming/main.vue
+++ src/components/games/programming/main.vue
@@ -451,7 +451,7 @@ export default {
     bottom: 0;
     width: 100%;
     height: 80px;
-    background-color: rgba(0, 0, 0, 0.6);
+    background-color: rgba(0, 0, 0, 0.7);
     margin: auto;
   }
 
