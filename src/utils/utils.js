4dd60249c580ab3d315cfc62d4499abfabffe8e2
Horoneru <horonerudev@gmail.com>
2018-03-02 09:29:07 +0100

    Refactor une fonction dans un util

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,12 @@
 export default {
   isObject: function(obj) {
     return obj === Object(obj);
+  },
+  gameAllDone: function(scores) {
+    const gameAllDone = Object.keys(scores).every(function(key) {
+      return (scores[key] !== null);
+    });
+
+    return gameAllDone;
   }
 };
